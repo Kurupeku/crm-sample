6aa3acb3654d04baced63c9819c56142fea04143
kurupeku <22340645+kurupeku@users.noreply.github.com>
2021-09-26 19:22:35 +0900

    create address model in user_api

--- user_api/db/schema.rb
+++ user_api/db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_26_051157) do
+ActiveRecord::Schema.define(version: 2021_09_26_084050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "postal_code"
+    t.string "prefecture"
+    t.string "city"
+    t.string "street"
+    t.string "building"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "company_name"
@@ -24,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_09_26_051157) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "addresses", "users"
 end
