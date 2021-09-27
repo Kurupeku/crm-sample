fee528913b0d31552f67831e4ba6a97898b2ff46
kurupeku <y.u.kurupeku@gmail.com>
2021-09-27 11:24:15 +0900

    add pagination to users in user_api

--- user_api/app/graphql/types/query_type.rb
+++ user_api/app/graphql/types/query_type.rb
@@ -6,7 +6,16 @@ module Types
 
     field :users, [UserType], null: false
     def users
-      User.all
+      User.all.order created_at: :desc
+    end
+
+    field :users_list, UsersListType, null: false do
+      argument :page, Int, required: false
+      argument :per, Int, required: false
+    end
+    def users_list(page: 1, per: 25)
+      result = User.all.order(created_at: :desc).page(page).per(per)
+      parse_connection_payload result, :users
     end
 
     field :user, UserType, null: true do
@@ -14,6 +23,20 @@ module Types
     end
     def user(id:)
       User.find id
+    end
+
+    private
+
+    def parse_connection_payload(result, result_key)
+      {
+        "#{result_key}": result,
+        page_info: {
+          current_page: result.current_page,
+          records_count: result.total_count,
+          pages_count: result.total_pages,
+          limit: result.limit_value
+        }
+      }
     end
   end
 end
