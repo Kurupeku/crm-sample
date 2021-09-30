36b3a4384b4ea40339d0d971e2a8cf615e9f26a3
kurupeku <y.u.kurupeku@gmail.com>
2021-09-30 20:16:13 +0900

    create Menu model and spec in inquiry_api

---
+++ inquiry_api/app/models/menu.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: menus
+#
+#  id           :bigint           not null, primary key
+#  name         :string           not null
+#  published_on :date
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+class Menu < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+
+  scope :published, lambda {
+    where.not published_on: nil
+  }
+end
