606b65a8a1abc765fef4e2809bdf4013e34bde2e
kurupeku <22340645+kurupeku@users.noreply.github.com>
2021-09-30 20:52:51 +0900

    craete menus controller and request spec in inquiry_api

--- inquiry_api/config/routes.rb
+++ inquiry_api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/menus' => 'menus#index', constraints: ->(req) { req.format == :json }
   post '/inquiries' => 'inquiries#create', constraints: ->(req) { req.format == :json }
   post '/graphql', to: 'graphql#execute'
   mount GraphiQL::Rails::Engine, at: '/', graphql_path: '/graphql' if Rails.env.development?
