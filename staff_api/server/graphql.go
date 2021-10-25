edace443ddacb9c1c6d97155ec914f84546e056c
kurupeku <22340645+kurupeku@users.noreply.github.com>
2021-10-25 10:50:29 +0900

    add health_check to staff_api

--- staff_api/server/graphql.go
+++ staff_api/server/graphql.go
@@ -6,6 +6,7 @@ import (
 
 	"staff_api/graph"
 	"staff_api/graph/generated"
+	"staff_api/handlers"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -16,6 +17,8 @@ const gqlPort = "3001"
 
 func RunGraphql(db *gorm.DB) {
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: db}}))
+
+	http.HandleFunc("/health_check", handlers.HealthCheckHandler)
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
 	http.Handle("/graphql", srv)
