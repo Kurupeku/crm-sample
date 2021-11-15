096af0783cbfdcc548e2678a4cde153f19242135
kurupeku <y.u.kurupeku@gmail.com>
2021-11-16 05:22:40 +0900

    fix type misses and normalize query spec codes

--- user_api/spec/requests/graphql/mutations/delete_user_spec.rb
+++ user_api/spec/requests/graphql/mutations/delete_user_spec.rb
@@ -5,9 +5,7 @@ require 'rails_helper'
 RSpec.describe Mutations::UpdateUser, type: :request do
   let(:user) { create :user }
   let(:user_attributes) { user.attributes.symbolize_keys }
-  let(:address) { create :address, user: user }
-  let(:address_attributes) { address.attributes.symbolize_keys }
-  def input(user, _address)
+  def input(user)
     <<~INPUT
       mutation {
         deleteUser(input: { id: "#{user[:id]}" }) {
@@ -18,18 +16,18 @@ RSpec.describe Mutations::UpdateUser, type: :request do
     INPUT
   end
 
-  describe '存在するIDのとき' do
+  describe '正常な値を与えた場合' do
     it 'レコードが削除される' do
-      result = AppSchema.execute input(user_attributes, address_attributes)
+      result = AppSchema.execute input(user_attributes)
       is_asserted_by { result.dig('data', 'deleteUser', 'name') == user.name }
       is_asserted_by { User.find_by_id(user.id).nil? }
     end
   end
 
-  describe '存在しないIDのとき' do
+  describe '存在しないIDを指定した場合' do
     it 'errorsフィールドにエラー内容が生成され、レコードは削除されない' do
       user_attributes[:id] = 0
-      result = AppSchema.execute input(user_attributes, address_attributes)
+      result = AppSchema.execute input(user_attributes)
       is_asserted_by { result.key?('errors') }
       is_asserted_by { User.find(user.id).present? }
     end
