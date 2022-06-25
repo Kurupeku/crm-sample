743f07831b4a0960f3010a1e8ddd28b2a08db2f3
kurupeku <y.u.kurupeku@gmail.com>
2022-06-26 05:40:39 +0900

    create grpc_staff_api

---
+++ grpc_staff_api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"staff_api/database"
+	"staff_api/entity"
+	"staff_api/server"
+)
+
+func main() {
+	if os.Getenv("GO_ENV") == "" {
+		os.Setenv("GO_ENV", "development")
+	}
+
+	if len(os.Args) != 1 {
+		log.Fatal("require argument of port number")
+	}
+
+	port := os.Args[1]
+	if _, err := strconv.Atoi(port); err != nil {
+		log.Fatal("env var PORT must be a uint")
+	}
+
+	db, _ := database.Connect()
+
+	if err := db.AutoMigrate(&entity.Staff{}); err != nil {
+		log.Fatal(err)
+	}
+
+	server.RunGrpc(port)
+}
---
+++ grpc_staff_api/server/grpc.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"staff_api/entity"
+	"staff_api/proto"
+
+	"google.golang.org/grpc"
+)
+
+type gsv struct {
+	proto.UnimplementedAuthServer
+}
+
+func (*gsv) Authenticate(ctx context.Context, req *proto.AuthenticateRequest) (*proto.AuthenticateResponse, error) {
+	email := req.GetEmail()
+	password := req.GetPassword()
+
+	var staff entity.Staff
+	return &proto.AuthenticateResponse{
+		Authenticated: staff.IsAuthenticated(email, password),
+	}, nil
+}
+
+func RunGrpc(ctx context.Context, port string) {
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	srv := grpc.NewServer()
+
+	proto.RegisterAuthServer(srv, &gsv{})
+	if err := srv.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
