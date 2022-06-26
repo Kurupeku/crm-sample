e54bc9f17e87e5c3f1c9ab00b850bf340bee71eb
kurupeku <y.u.kurupeku@gmail.com>
2022-06-27 00:08:53 +0900

    add db_name selector

--- batch/database/user_api/postgres.go
+++ batch/database/user_api/postgres.go
@@ -1,8 +1,9 @@
-package user_api
+package database
 
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 
 	"gorm.io/driver/postgres"
@@ -19,42 +20,69 @@ type Meta struct {
 	password string
 	port     string
 	dbname   string
+	ssl      bool
 }
 
-func Connect() (*gorm.DB, error) {
-	m := Meta{}
-	m.init()
-	db, err := connect(&m)
-
-	if err != nil {
-		return nil, err
+func newMeta() *Meta {
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		log.Fatal("env 'DB_HOST' is required")
 	}
 
-	d = db
-
-	if err != nil {
-		return nil, err
+	user := os.Getenv("DB_USER")
+	if user == "" {
+		log.Fatal("env 'DB_USER' is required")
 	}
 
-	return db, nil
+	password := os.Getenv("DB_PASSWORD")
+	if password == "" {
+		log.Fatal("env 'DB_PASSWORD' is required")
+	}
+
+	port := os.Getenv("DB_PORT")
+	if port == "" {
+		log.Fatal("env 'DB_PORT' is required")
+	}
+
+	dbname := os.Getenv("DB_NAME")
+	if dbname == "" {
+		dbname = "user_" + os.Getenv("GO_ENV")
+	}
+
+	var ssl bool
+	if s := os.Getenv("DB_SSL"); s == "true" {
+		ssl = true
+	}
+
+	return &Meta{
+		host:     host,
+		user:     user,
+		password: password,
+		port:     port,
+		dbname:   dbname,
+		ssl:      ssl,
+	}
 }
 
-func GetDB() *gorm.DB {
-	return d
-}
+func (m *Meta) sslmode() string {
+	if m.ssl {
+		return "require"
+	}
 
-func (m *Meta) init() {
-	m.host = os.Getenv("DB_HOST")
-	m.user = os.Getenv("DB_USER")
-	m.password = os.Getenv("DB_PASSWORD")
-	m.port = os.Getenv("DB_PORT")
-	m.dbname = "user_" + os.Getenv("GO_ENV")
+	return "disable"
 }
 
 func (m *Meta) pgDsn() string {
 	return fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Tokyo",
-		m.host, m.port, m.user, m.password, m.dbname,
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Tokyo",
+		m.host, m.user, m.password, m.dbname, m.port, m.sslmode(),
+	)
+}
+
+func (m *Meta) pgDsnWithoutDBName() string {
+	return fmt.Sprintf(
+		"host=%s user=%s password=%s port=%s sslmode=%s TimeZone=Asia/Tokyo",
+		m.host, m.user, m.password, m.port, m.sslmode(),
 	)
 }
 
@@ -67,10 +95,33 @@ func connect(m *Meta) (*gorm.DB, error) {
 	return nil, errors.New("const 'database' is unmatched in method connect")
 }
 
+func Connect() (*gorm.DB, error) {
+	m := newMeta()
+
+	db, err := connect(m)
+	if err != nil {
+		return nil, err
+	}
+
+	d = db
+
+	return db, nil
+}
+
+func GetDB() *gorm.DB {
+	return d
+}
+
 func connectOrCreatePostgresDatabase(m *Meta) (*gorm.DB, error) {
 	named, err := gorm.Open(postgres.Open(m.pgDsn()), &gorm.Config{})
 	if err != nil {
-		return nil, err
+		public, _ := gorm.Open(postgres.Open(m.pgDsnWithoutDBName()), &gorm.Config{})
+		public.Exec(fmt.Sprintf("CREATE DATABASE %s;", m.dbname))
+
+		named, err = gorm.Open(postgres.Open(m.pgDsn()), &gorm.Config{})
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return named, nil
