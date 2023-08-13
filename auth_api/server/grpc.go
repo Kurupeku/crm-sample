fde7fd2c819ee24234831578de3f4ccb777f81c5
kurupeku <22340645+kurupeku@users.noreply.github.com>
2023-08-13 15:57:44 +0900

    feat: workspace init

---
+++ auth_api/server/grpc.go
@@ -0,0 +1,43 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"staff_api/entity"
+	"staff_api/proto"
+
+	"google.golang.org/grpc"
+)
+
+const grpcPort = "50051"
+
+type gsv struct {
+	proto.UnimplementedAuthServer
+}
+
+func (*gsv) Authenticate(ctx context.Context, req *proto.AuthenticateRequest) (*proto.AuthenticateResponse, error) {
+	email := req.GetEmail()
+	password := req.GetPassword()
+
+	var staff entity.Staff
+	return &proto.AuthenticateResponse{
+		Authenticated: staff.IsAuthenticated(email, password),
+	}, nil
+}
+
+func RunGrpc() {
+	listener, err := net.Listen("tcp", ":"+grpcPort)
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	srv := grpc.NewServer()
+
+	proto.RegisterAuthServer(srv, &gsv{})
+	if err := srv.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
