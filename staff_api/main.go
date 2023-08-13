f8f3973325c3e63bad53aec68a66a287dca0b3ac
kurupeku <22340645+kurupeku@users.noreply.github.com>
2023-08-14 04:02:39 +0900

    feat: remove grpc from staff_api

--- staff_api/main.go
+++ staff_api/main.go
@@ -2,24 +2,44 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"staff_api/database"
 	"staff_api/entity"
-	"staff_api/server"
+	"staff_api/graph"
+	"staff_api/graph/generated"
+	"staff_api/handlers"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
+const gqlPort = "3001"
+
 func main() {
-	if os.Getenv("GO_ENV") == "" {
-		os.Setenv("GO_ENV", "development")
-	}
-
 	db, _ := database.Connect()
-
 	if err := db.AutoMigrate(&entity.Staff{}); err != nil {
 		log.Fatal(err)
 	}
+	runServer()
+}
 
-	go server.RunGrpc()
-	server.RunGraphql()
+func runServer() {
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/health_check", http.HandlerFunc(handlers.HealthCheckHandler))
+	http.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
+	http.Handle("/graphql", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", gqlPort)
+	if err := http.ListenAndServe(":"+gqlPort, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func init() {
+	if _, ok := os.LookupEnv("GO_ENV"); !ok {
+		os.Setenv("GO_ENV", "development")
+	}
 }
