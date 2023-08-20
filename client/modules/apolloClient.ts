43469e7fc8e8bdf4e3421e04365a41c92b0b0426
kurupeku <22340645+kurupeku@users.noreply.github.com>
2023-08-20 22:01:49 +0900

    feat: modify endpoint in apollo client (#91)

--- client/modules/apolloClient.ts
+++ client/modules/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: `${process.env.NEXT_PUBLIC_API_HOST}/api/graphql`,
+  uri: `${process.env.NEXT_PUBLIC_API_HOST || ""}/api/graphql`,
 });
 
 const authLink = setContext((_, { headers }) => {
